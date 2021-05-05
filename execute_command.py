b8be9a3bcd1a498fbb998dab008bcc81cccb8682
Paul St. Amour <paul35444@aol.com>
2021-05-04 19:53:07 -0500

    +added args to the func to call

--- execute_command.py
+++ execute_command.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import subprocess, smtplib
 
-def send_mail()
+def send_mail(email, password, message):
+    
 
 command = "msg * words words words"
 subprocess.Popen(command, shell=True)
