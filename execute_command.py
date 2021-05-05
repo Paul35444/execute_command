0762cddfa29b8a971f84de6c698261765f76951d
Paul St. Amour <paul35444@aol.com>
2021-05-04 19:55:10 -0500

    +address for google's smtp server and port to the server var

--- execute_command.py
+++ execute_command.py
@@ -2,7 +2,8 @@
 import subprocess, smtplib
 
 def send_mail(email, password, message):
-    server = smtplib.SMTP()
+#use google SMTP server and port 587 (google port for smtp)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
 
 command = "msg * words words words"
 subprocess.Popen(command, shell=True)
